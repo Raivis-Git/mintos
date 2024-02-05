e7dcce27cfc206cc1c4063a63909ea98761acf54
Raivis <raivukam2@gmail.com>
2024-02-05 06:55:15 +0200

    bad request check

--- src/main/java/com/mintos/task/controller/AccountController.java
+++ src/main/java/com/mintos/task/controller/AccountController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 
@@ -21,11 +22,26 @@ public class AccountController {
 
     @GetMapping("/client/{clientId}")
     public ResponseEntity<?> getAccountsByClientId(@PathVariable Long clientId) {
+        if (clientId == null)
+            ResponseEntity.badRequest().build();
+
         return ResponseEntity.ok(accountService.fetchAccountsByClientId(clientId));
     }
 
     @PostMapping()
     public ResponseEntity<?> addAccount(@RequestBody AccountCreateRequest accountCreateRequest) {
+            if (accountCreateRequest.getIso() == null && accountCreateRequest.getIso().isBlank())
+                return ResponseEntity.badRequest().body("Please provide account currency iso");
+
+            if (accountCreateRequest.getBalance() == null)
+                return ResponseEntity.badRequest().body("Please provide account balance");
+
+            if (accountCreateRequest.getBalance().compareTo(BigDecimal.ZERO) < 0)
+                return ResponseEntity.badRequest().body("Please provide not negative balance");
+
+            if (accountCreateRequest.getClientId() == null)
+                return ResponseEntity.badRequest().body("Please provide account client id");
+
         try {
             Account account = accountService.addAccount(accountCreateRequest.iso, accountCreateRequest.balance, accountCreateRequest.clientId);
             return ResponseEntity.ok(account);
@@ -36,6 +52,15 @@ public class AccountController {
 
     @PostMapping("balance/transfer")
     public ResponseEntity<?> transferFunds(@RequestBody TransferFundsRequest transferFundsRequest) {
+            if (transferFundsRequest.getAmount() == null || transferFundsRequest.getAmount().compareTo(BigDecimal.ZERO) < 0)
+                return ResponseEntity.badRequest().body("Amount has to be positive");
+
+            if (transferFundsRequest.getCurrencyIso() == null && transferFundsRequest.getCurrencyIso().isBlank())
+                return ResponseEntity.badRequest().body("Please provide currency");
+
+            if (transferFundsRequest.getToAccountId() == null || transferFundsRequest.getFromAccountId() == null)
+                return ResponseEntity.badRequest().body("Both accounts for transfer must be provided");
+
         try {
             List<Account> accountList = accountService.transferFunds(
                     transferFundsRequest.fromAccountId, transferFundsRequest.toAccountId,
@@ -49,6 +74,9 @@ public class AccountController {
 
     @GetMapping(value = "transactions/{accountId}")
     public ResponseEntity<?> getAccountTransactions(@PathVariable Long accountId, @RequestParam Integer limit, @RequestParam Integer offset) {
+        if (accountId == null)
+            return ResponseEntity.badRequest().body("Please provide account id");
+
         List<Transaction> transactionList = accountService.fetchAccountTransactions(accountId, limit, offset);
         return ResponseEntity.ok(transactionList);
     }
